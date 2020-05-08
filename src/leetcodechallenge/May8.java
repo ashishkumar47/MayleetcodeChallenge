da83b85066d591d0205dfc65588d3f2e4b69fc78
ashishkumar47 <ashishkumar8163022@gmail.com>
2020-05-09 03:50:06 +0530

    May8:changes in logic

--- src/leetcodechallenge/May8.java
+++ src/leetcodechallenge/May8.java
@@ -8,8 +8,8 @@ public class May8 {
 		int xOne = coordinates[0][0], yOne = coordinates[0][1], xTwo = coordinates[1][0], yTwo = coordinates[1][1];
 		for (int i = 2; i < coordinates.length; i++) {
 			int xThree = coordinates[i][0], yThree = coordinates[i][1];
-			int a = (yThree - yTwo), b = (xThree - xTwo), c = (yThree - yOne), d = (xThree - xOne),
-					m1 = Integer.MAX_VALUE, m2 = Integer.MAX_VALUE;
+			int a = (yTwo - yOne), b = (xTwo - xOne), c = (yThree - yTwo), d = (xThree - xTwo), m1 = Integer.MAX_VALUE,
+					m2 = Integer.MAX_VALUE;
 			if (b != 0)
 				m1 = a / b;
 			if (d != 0)
